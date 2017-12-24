aadd5d68b2f161365503c8babe1d8bf9b9103cbb
Kaloian Ianev <kianev@gmail.com>
2017-12-24 19:02:33 +0200

    work on components, form added

--- recipe-book/src/app/recepies/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recepies/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'This is a test recipe',
+      'https://static.pexels.com/photos/691114/pexels-photo-691114.jpeg'),
+    new Recipe(
+      'Test Recipe',
+      'This is a test recipe',
+      'https://static.pexels.com/photos/691114/pexels-photo-691114.jpeg')
+  ];
   constructor() { }
 
   ngOnInit() {
