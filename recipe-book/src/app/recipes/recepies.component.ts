52df891be4c7a1c42f9f1a6e6331b74555b45999
Kaloian Ianev <kianev@gmail.com>
2018-01-05 11:25:33 +0200

    Reactive driven approach for editing recipe form

--- recipe-book/src/app/recipes/recepies.component.ts
+++ recipe-book/src/app/recipes/recepies.component.ts
@@ -1,11 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {RecipeService} from "./recipe.service";
 
 @Component({
   selector: 'app-recepies',
   templateUrl: './recepies.component.html',
   styleUrls: ['./recepies.component.css'],
-  providers: [RecipeService]
 })
 export class RecipesComponent implements OnInit {
 
--- recipe-book/src/app/recipes/recipe.service.ts
+++ recipe-book/src/app/recipes/recipe.service.ts
@@ -2,27 +2,29 @@ import {Recipe} from "./recipe.model";
 import {Injectable} from "@angular/core";
 import {Ingredient} from "../shared/ingredient.model";
 import {ShoppingListService} from "../shopping-list/shopping-list.service";
+import {Subject} from "rxjs/Subject";
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
 
   private recipes: Recipe[] = [
     new Recipe(
       'Eggs in Panagurishte',
       'Eggs with cheese in sauce',
-      'https://static.pexels.com/photos/691114/pexels-photo-691114.jpeg',
+      'http://www.kulinarno-joana.com/wp-content/uploads/2010/10/DSC6529.jpg',
       [
         new Ingredient('Eggs', 4),
-        new Ingredient('Cheese', 0.2)
+        new Ingredient('Cheese', 1)
       ]),
 
     new Recipe(
       'Bread',
       'Make a delicious bread',
-      'https://static.pexels.com/photos/9510/food-pizza-kitchen-recipe.jpg',
+      'https://s.rozali.com/p/h/l/hlqb-pitka-pechene-(3)-102093-500x334.jpg',
       [
         new Ingredient('Flour', 1),
-        new Ingredient('Water', 0.3)])
+        new Ingredient('Water', 1)])
   ];
 
   constructor(private slService: ShoppingListService){}
@@ -37,5 +39,20 @@ export class RecipeService {
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
    this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
