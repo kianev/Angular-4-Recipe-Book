45690001db77a878e1264b3c069c68adc3f815b0
Kaloian Ianev <kianev@gmail.com>
2018-01-08 21:29:50 +0200

    Firebase email authentication added, protected routes

--- recipe-book/src/app/app.component.ts
+++ recipe-book/src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements  OnInit {
   loadedFeature = 'recipe';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyA7JrenudgHmf9aZ8H-yM2CE-YbNfKwGAc",
+      authDomain: "angular4-recipe-book-ae13e.firebaseapp.com"
+    })
+  }
 
   onNavigate(feature: string) {
    this.loadedFeature = feature;
--- recipe-book/src/app/shared/data-storage.service.ts
+++ recipe-book/src/app/shared/data-storage.service.ts
@@ -3,17 +3,20 @@ import {Http, Headers, Response} from '@angular/http';
 import {RecipeService} from "../recipes/recipe.service";
 import {Recipe} from "../recipes/recipe.model";
 import 'rxjs/Rx';
+import {AuthService} from "../auth/auth.service";
 
 @Injectable()
 export class DataStorageService {
-  constructor(private http: Http, private recipeService: RecipeService) {}
+  constructor(private http: Http, private recipeService: RecipeService, private authService: AuthService) {}
 
   storeRecipe() {
-    return this.http.put('https://angular4-recipe-book-ae13e.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+    const token = this.authService.getToken();
+    return this.http.put('https://angular4-recipe-book-ae13e.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
   }
 
   getRecipes() {
-    this.http.get('https://angular4-recipe-book-ae13e.firebaseio.com/recipes.json')
+    const token = this.authService.getToken();
+    this.http.get('https://angular4-recipe-book-ae13e.firebaseio.com/recipes.json?auth=' + token)
       .map(
         (response: Response) => {
           const recipes: Recipe[] = response.json();
