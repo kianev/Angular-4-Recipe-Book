60c9b8ecc893d7a0ddd46ee1f84cfe3f9eb94428
Kaloian Ianev <kianev@gmail.com>
2018-01-09 18:13:38 +0200

    Using Modules for app optimization, lazy loading, Ahead of Time compilation for production

--- recipe-book/src/app/core/header/header.component.ts
+++ recipe-book/src/app/core/header/header.component.ts
@@ -1,14 +1,14 @@
 import {Component} from "@angular/core";
-import {DataStorageService} from "../shared/data-storage.service";
+import {DataStorageService} from "../../shared/data-storage.service";
 import {Response} from "@angular/http";
-import {AuthService} from "../auth/auth.service";
+import {AuthService} from "../../auth/auth.service";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-  constructor(private dataStorageService: DataStorageService, private authService: AuthService) {}
+  constructor(private dataStorageService: DataStorageService, public authService: AuthService) {}
 
   onSaveData() {
    this.dataStorageService.storeRecipe()
